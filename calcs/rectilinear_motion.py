b905da5aeb2f7848fa220d91569719f85e12c562
DEENUU1 <kacperwlodarczyk@protonmail.com>
2023-12-10 20:40:17 +0100

    Add function to calculate average acceleration

--- calcs/rectilinear_motion.py
+++ calcs/rectilinear_motion.py
@@ -3,9 +3,15 @@ from typing import Optional
 
 
 def calculate_displacement(xs: float, xe: float) -> float:
+    # Xe - Xs
     return xe - xs
 
 
 def calculate_average_speed(xe: float, xs: float, te: float, ts: float) -> float:
+    # Xe - Xs / Te - Ts
     return calculate_displacement(xs, xe) / (te - ts)
 
+
+def calculate_avg_acceleration(ve: float, vs: float, te: float, ts: float) -> float:
+    # Ve - Vs / Te - Ts
+    return (ve - vs) / (te - ts)
