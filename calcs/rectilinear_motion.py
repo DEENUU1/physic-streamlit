3357cf88996021fcce17295a53c9a779ea6d86e1
DEENUU1 <kacperwlodarczyk@protonmail.com>
2023-12-10 20:54:27 +0100

    Add functions to calculate time, speed and trajectory of free fall

--- calcs/rectilinear_motion.py
+++ calcs/rectilinear_motion.py
@@ -1,5 +1,6 @@
 import math
 from typing import Optional
+import pandas as pd
 
 
 def calculate_displacement(xs: float, xe: float) -> float:
@@ -15,3 +16,26 @@ def calculate_average_speed(xe: float, xs: float, te: float, ts: float) -> float
 def calculate_avg_acceleration(ve: float, vs: float, te: float, ts: float) -> float:
     # Ve - Vs / Te - Ts
     return (ve - vs) / (te - ts)
+
+
+def calculate_free_fall_total_time(h: float, g: float = 9.81) -> float:
+    # sqrt(2*h / g)
+    return math.sqrt((2*h) / g)
+
+
+def calculate_free_fall_ending_speed(h: float, g: float = 9.81) -> float:
+    # sqrt(2*h*g)
+    return math.sqrt(2*h*g)
+
+
+def free_fall_trajectory(t: float, g: float = 9.81) -> pd.DataFrame:
+    times = []
+    speeds = []
+
+    for t in range(int(t) + 1):
+        time = float(t)
+        speed = g * time
+        times.append(time)
+        speeds.append(speed)
+
+    return pd.DataFrame({'time': times, 'speed': speeds})
