2292d25f111e35ac48a1c44a40f2c3e51ec9d332
DEENUU1 <kacperwlodarczyk@protonmail.com>
2023-12-10 14:56:05 +0100

    Create method to get position at specified time and return trajectory dataframe

--- horizontal.py
+++ horizontal.py
@@ -1,5 +1,7 @@
 import math
-from typing import Optional, Dict, Any
+from typing import Optional, Dict, Any, Tuple
+import numpy as np
+import pandas as pd
 
 
 class HorizontalProjection:
@@ -37,6 +39,14 @@ class HorizontalProjection:
             self.t = math.sqrt((2 * self.h0) / self.g)
             self.v0 = self.s / self.t
 
+    def get_position_at_time(self, time: float) -> Tuple[float, float]:
+        if self.v0 is not None and self.t is not None:
+            x = self.v0 * time
+            y = self.h0 - (0.5 * self.g * pow(time, 2))
+            return x, y
+        else:
+            raise ValueError("You need to calculate v0 and t first.")
+
     def calc(self) -> Dict[str, Any]:
         return {
             "v0": self.v0,
@@ -46,6 +56,21 @@ class HorizontalProjection:
         }
 
 
+def create_trajectory_dataframe(hp: HorizontalProjection, num_points: int = 1000) -> pd.DataFrame:
+    max_time = hp.t
+    times = np.linspace(0, max_time, num=num_points)
+    positions = [hp.get_position_at_time(t) for t in times]
+    x_values, y_values = zip(*positions)
+
+    data = {'Time': times, 'X': x_values, 'Y': y_values}
+    df = pd.DataFrame(data)
+
+    return df
+
+
 if __name__ == '__main__':
-    hp = HorizontalProjection(v0=10, t=10)
+    hp = HorizontalProjection(v0=20, t=3.5)
     print(hp.calc())
+
+    trajectory_df = create_trajectory_dataframe(hp)
+    print(trajectory_df)
