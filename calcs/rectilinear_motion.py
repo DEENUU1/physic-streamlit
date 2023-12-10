dc9c09250070c34a8da22757e2f97a225599e8c3
DEENUU1 <kacperwlodarczyk@protonmail.com>
2023-12-10 20:37:16 +0100

    Add function to calculate average speed

--- calcs/rectilinear_motion.py
+++ calcs/rectilinear_motion.py
@@ -6,5 +6,6 @@ def calculate_displacement(xs: float, xe: float) -> float:
     return xe - xs
 
 
-
+def calculate_average_speed(xe: float, xs: float, te: float, ts: float) -> float:
+    return calculate_displacement(xs, xe) / (te - ts)
 
